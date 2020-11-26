6c7e5bcd37912e6abd400b870605a205c783fd25
nurai <nuraimaratova50@gmail.com>
2020-11-26 12:15:21 +0600

    minor fix

--- comments_service/api/serializers.py
+++ comments_service/api/serializers.py
@@ -3,12 +3,9 @@ from rest_framework import serializers
 from posts.models import Post
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Post
         fields = ['title', 'slug', 'body', 'date_pub']
         read_only_fields = ['slug']
-        extra_kwargs = {
-            'url': {'lookup_field': 'slug'}
-        }
