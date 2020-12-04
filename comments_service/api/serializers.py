2f93c50ee13e1fc345fec649e2200583e13e3ca3
nurai <nuraimaratova50@gmail.com>
2020-12-04 18:06:11 +0600

    minor fix

--- comments_service/api/serializers.py
+++ comments_service/api/serializers.py
@@ -27,7 +27,6 @@ class PostSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
 
-
     class Meta:
         model = Comment
         fields = ['id', 'content', 'post']
